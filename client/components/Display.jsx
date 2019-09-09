d2fd5c4799756745b2d06b6321da320334dee815
julyseng <julyseng@gmail.com>
2019-09-09 20:33:07 +1200

    add card

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -4,69 +4,14 @@ import React from 'react'
 function Display() {
     return (
         <React.Fragment>
-            <div className="card">
-                <div className="card-content">
-                    <p className="subtitle">
-                        “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
-                    <p className="subtitle">
-                        Jeff Atwood </p>
+            <div className='card'>
+                <div className='card-image'>
+                    <img src="../../castle.jpg" alt="castle" />
                 </div>
-                <footer className="card-footer">
-                    <p className="card-footer-item">
-                        <span>
-                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                        </span>
-                    </p>
-                    <p className="card-footer-item">
-                        <span>
-                            Share on <a href="#">Facebook</a>
-                        </span>
-                    </p>
-                </footer>
-            </div>
-
-        {/* second div section  */}
-        <div className="card">
-                <div className="card-content">
-                    <p className="subtitle">
-                        “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
-                    <p className="subtitle">
-                        Jeff Atwood </p>
+                <div className="media-content">
+                    <p className="h3">John Smith</p>
+                    <p className="h4">@johnsmith</p>
                 </div>
-                <footer className="card-footer">
-                    <p className="card-footer-item">
-                        <span>
-                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                        </span>
-                    </p>
-                    <p className="card-footer-item">
-                        <span>
-                            Share on <a href="#">Facebook</a>
-                        </span>
-                    </p>
-                </footer>
-            </div>
-        
-        {/* third div section  */}
-        <div className="card">
-                <div className="card-content">
-                    <p className="subtitle">
-                        “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</p>
-                    <p className="subtitle">
-                        Jeff Atwood </p>
-                </div>
-                <footer className="card-footer">
-                    <p className="card-footer-item">
-                        <span>
-                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                        </span>
-                    </p>
-                    <p className="card-footer-item">
-                        <span>
-                            Share on <a href="#">Facebook</a>
-                        </span>
-                    </p>
-                </footer>
             </div>
 
         </React.Fragment>
--- client/components/test.js
+++ client/components/test.js
@@ -1,23 +1,21 @@
-<div className="card">
-  <div className="card-image">
-    <figure className="image is-4by3">
+<div class="card">
+  <div class="card-image">
       <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-    </figure>
   </div>
-  <div className="card-content">
-    <div className="media">
-      <div className="media-left">
-        <figure className="image is-48x48">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
           <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
         </figure>
       </div>
-      <div className="media-content">
-        <p className="title is-4">John Smith</p>
-        <p className="subtitle is-6">@johnsmith</p>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
       </div>
     </div>
 
-    <div className="content">
+    <div class="content">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       Phasellus nec iaculis mauris. <a>@bulmaio</a>.
       <a href="#">#css</a> <a href="#">#responsive</a>
